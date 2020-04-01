a31acf1c13ecde1ccab4faf7b91a7e7a2114601c
JelloJordan <55707792+JelloJordan@users.noreply.github.com>
2020-03-31 17:42:33 -0700

    Vertex Struct

--- InvarEngine/Game.cs
+++ InvarEngine/Game.cs
@@ -6,14 +6,52 @@ using System.Drawing;
 namespace InvarEngine
 {
 
+    struct Vertex
+    {
+        public Vector2 position;
+        public Vector2 texCoord;
+        public Vector4 color;
+
+        public Color Color
+        {
+            get
+            {
+                return Color.FromArgb((int)(255 * color.W), (int)(255 * color.X), (int)(255 * color.Y), (int)(255 * color.Z));
+            }
+            set
+            {
+                this.color = new Vector4(value.R/255f, value.G/255f, value.B/255f, value.A/255f);
+            }
+        }
+
+        public static int SizeInBytes
+        {
+
+            get { return Vector2.SizeInBytes * 2 + Vector4.SizeInBytes; } //update when values get added to vertex struct
+
+        }
+
+        public Vertex(Vector2 position, Vector2 texCoord)
+        {
+
+            this.position = position;
+            this.texCoord = texCoord;
+            this.color = new Vector4(1, 1, 1, 1);
+
+        }
+
+    }
+
     class Game
     {
 
         public GameWindow window;
         Texture2D texture;
 
-        Vector2[] vertBuffer;
+        Vertex[] vertBuffer;
         int VBO;
+        uint[] indexBuffer;
+        int IBO; //Hold ID for index buffer object
 
         public Game(GameWindow windowInput)
         {
@@ -35,20 +73,34 @@ namespace InvarEngine
             texture = ContentPipe.LoadTexture("Icon.png");
 
             
-            vertBuffer = new Vector2[8]
+            vertBuffer = new Vertex[6]
             {
+                //new Vertex(new Vector2(0,0), new Vector2(0,0)) {Color = Color.Red},  if wanting to set color or other vars
+                new Vertex(new Vector2(0,0), new Vector2(0,0)),
+                new Vertex(new Vector2(100,0), new Vector2(1,0)),
+                new Vertex(new Vector2(100,100), new Vector2(1,1)),
+                new Vertex(new Vector2(0,100), new Vector2(0,1)),
 
-                new Vector2(-.5f, .5f), new Vector2(0, 0), 
-                new Vector2(-.5f, -.5f), new Vector2(0, 1), 
-                new Vector2(.5f, -.5f), new Vector2(1, 1), 
-                new Vector2(.5f, .5f), new Vector2(1, 0), 
-
+                new Vertex(new Vector2(0,200), new Vector2(0,0)),
+                new Vertex(new Vector2(100,200), new Vector2(1,0))
             };
 
             VBO = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
-            GL.BufferData<Vector2>(BufferTarget.ArrayBuffer, (IntPtr)(Vector2.SizeInBytes * vertBuffer.Length), vertBuffer, BufferUsageHint.StaticDraw);
+            GL.BufferData<Vertex>(BufferTarget.ArrayBuffer, (IntPtr)(Vertex.SizeInBytes * vertBuffer.Length), vertBuffer, BufferUsageHint.StaticDraw);
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            indexBuffer = new uint[8]       //8 size for room for two quads
+            {
+                0, 1, 2, 3,     //right side up quad
+
+                4, 5, 2, 3      //upsidedown quad
+            };
+
+            IBO = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, IBO);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(sizeof(uint) * indexBuffer.Length), indexBuffer, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
 
         }
 
@@ -75,7 +127,13 @@ namespace InvarEngine
             GL.Enable(EnableCap.Blend);
             GL.Enable(EnableCap.Texture2D);
             GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            Matrix4 proj = Matrix4.CreateOrthographicOffCenter(0, window.Width, window.Height, 0, 0, 1);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref proj);
             
+            
+
             GL.BindTexture(TextureTarget.Texture2D, texture.ID);
 
             /*
@@ -98,13 +156,42 @@ namespace InvarEngine
 
             //GL.Disable(EnableCap.Texture2D);            //stop drawing textures for this next object
 
-            GL.EnableClientState(ArrayCap.VertexArray);    //We have an array of vertices   
-            GL.EnableClientState(ArrayCap.TextureCoordArray);        //We have texture coordinates
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);       //Tell OpenGL to look into memory and find this in the GPU
-            GL.VertexPointer(2, VertexPointerType.Float, Vector2.SizeInBytes * 2, 0); //Our verts are two floats, size in bytes is step size. Start at [0]
-            GL.TexCoordPointer(2, TexCoordPointerType.Float, Vector2.SizeInBytes * 2, Vector2.SizeInBytes); //Start at [1] by offsetting by size in bytes
+            //GL.EnableClientState(ArrayCap.VertexArray);    //We have an array of vertices   
+            //GL.EnableClientState(ArrayCap.TextureCoordArray);        //We have texture coordinates
+            //GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);       //Tell OpenGL to look into memory and find this in the GPU
+            //GL.VertexPointer(2, VertexPointerType.Float, Vector2.SizeInBytes * 2, 0); //Our verts are two floats, size in bytes is step size. Start at [0]
+            //GL.TexCoordPointer(2, TexCoordPointerType.Float, Vector2.SizeInBytes * 2, Vector2.SizeInBytes); //Start at [1] by offsetting by size in bytes
             
-            GL.DrawArrays(PrimitiveType.Quads, 0, vertBuffer.Length/2);       //Draw it
+            //GL.DrawArrays(PrimitiveType.Quads, 0, vertBuffer.Length/2);       //Draw it
+
+            Matrix4 world = Matrix4.CreateTranslation(100, 100, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref world);
+
+            GL.EnableClientState(ArrayCap.ColorArray);
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.EnableClientState(ArrayCap.TextureCoordArray);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+            GL.VertexPointer(2, VertexPointerType.Float, Vertex.SizeInBytes, (IntPtr)0);
+            GL.TexCoordPointer(2, TexCoordPointerType.Float, Vertex.SizeInBytes, (IntPtr)(Vector2.SizeInBytes * 1));
+            GL.ColorPointer(4, ColorPointerType.Float, Vertex.SizeInBytes, (IntPtr)(Vector2.SizeInBytes * 2));
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, IBO);
+            GL.DrawElements(PrimitiveType.Quads, indexBuffer.Length, DrawElementsType.UnsignedInt, 0);      //6 vertices but 2 quads
+
+            //GL.DrawArrays(PrimitiveType.Quads, 0, vertBuffer.Length);
+
+
+
+            /*world = Matrix4.CreateTranslation(200, 100, 0);         //drawing the object multiple times by offsetting world matrix and drawing same object from memory
+            GL.LoadMatrix(ref world);
+            GL.DrawArrays(PrimitiveType.Quads, 0, vertBuffer.Length);
+
+            world = Matrix4.CreateTranslation(300, 100, 0);
+            GL.LoadMatrix(ref world);
+            GL.DrawArrays(PrimitiveType.Quads, 0, vertBuffer.Length);*/
+
 
             GL.Flush();
             window.SwapBuffers();
